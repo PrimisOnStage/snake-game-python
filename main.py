99cc6a7f8f2fdc45d0f7ee8e80d14cde0e7294a1
Pratham Takoliya <pratham.limitless@gmail.com>
2025-07-22 15:25:49 +0530

    make better class sections

--- main.py
+++ main.py
@@ -30,4 +30,5 @@ while game:
 
 
 
+
 sc.exitonclick()
--- snake.py
+++ snake.py
@@ -1,20 +1,19 @@
 import turtle as t
+STARTING_POSITIONS = [0, -20, -40]
 
 class Snake:
 
     def __init__(self):
-        head = t.Turtle("square")
-        head.color("white")
-        head.penup()
-        self.seg_list = [head]
-        self.seg_loc = [-20, -40]
+        self.seg_list = []
+        self.create_snake()
 
-        for i in range(2):
+    def create_snake(self):
+        for i in range(len(STARTING_POSITIONS)):
             new_seg = t.Turtle("square")
             new_seg.color("white")
             new_seg.penup()
             self.seg_list.append(new_seg)
-            new_seg.goto(self.seg_loc[i], 0)
+            new_seg.goto(STARTING_POSITIONS[i], 0)
 
     def move(self):
         for i in range(len(self.seg_list) - 1, 0, -1):
