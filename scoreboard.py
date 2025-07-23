be35d3e70811813606e38303630d21835d2a0537
PrimisOnStage <pratham.limitless@gmail.com>
2025-07-23 09:01:29 +0530

    add scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.pencolor("white")
+        self.score = 0
+        self.goto(0,270)
+        self.refresh()
+
+    def score_up(self):
+        self.score += 1
+
+    def refresh(self):
+        self.clear()
+        self.write(f"Score = {self.score} ", False, align="center")
+
+
+
